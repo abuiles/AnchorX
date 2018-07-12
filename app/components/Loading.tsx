1e762dd4057de4971339241bfd23a7682ae56428
Adolfo Builes <builes.adolfo@gmail.com>
2018-07-12 12:15:13 -0500

    Add Loading component.

---
+++ app/components/Loading.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import {
+  ActivityIndicator,
+  StyleSheet,
+  StatusBar,
+  View
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+})
+
+class Loading extends React.Component {
+  render () {
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <StatusBar barStyle="default" />
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    )
+  }
+}
+
+export default Loading
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -5,6 +5,9 @@ import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import Button from './Button';
+import Welcome from './Welcome';
+import Loading from '../../app/components/Loading';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Loading', module).add('default', () => <Loading />);
