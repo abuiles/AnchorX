17390e42b0eedab5ac06453a5379d0b2a6b79446
Adolfo Builes <builes.adolfo@gmail.com>
2018-07-13 12:21:05 -0500

    Add Login component.

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Text } from 'react-native'
 
 import { storiesOf } from '@storybook/react-native'
 import { action } from '@storybook/addon-actions'
@@ -7,7 +6,12 @@ import { linkTo } from '@storybook/addon-links'
 
 import Welcome from './Welcome'
 import Loading from '../../app/components/Loading'
+import Login from './Login'
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
 
 storiesOf('Loading', module).add('default', () => <Loading />)
+
+storiesOf('Login', module)
+  .add('error', () => <Login withError={true} />)
+  .add('success', () => <Login withError={false} />)
