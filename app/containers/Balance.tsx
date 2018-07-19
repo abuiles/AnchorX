6bc0bdb3a0f77e773919d35b45d5b7a30706ed5d
Adolfo Builes <builes.adolfo@gmail.com>
2018-07-19 10:58:11 -0500

    Add streaming to balance container.

--- app/containers/Balance.tsx
+++ app/containers/Balance.tsx
@@ -32,14 +32,49 @@ export default class Balance extends React.Component<Props, State> {
     this.state = {}
   }
 
+  // Update
   async componentDidMount() {
     const { accountId } = this.props
     const stellarServer = new Server('https://horizon-testnet.stellar.org')
     const sdkAccount = await stellarServer.loadAccount(accountId)
 
+    // Setup streaming to the accountId, this returns a function which
+    // you can use to close the stream.
+    let accountEventsClose = stellarServer.accounts().accountId(accountId).stream({
+      // onmessage is called each time the ledger closes
+      onmessage: res => {
+        const { sdkAccount } = this.state
+
+        // Check if balances changed and if they did update sdkAcount.balances
+        if (sdkAccount.balances !== res.balances) {
+          sdkAccount.balances = res.balances
+
+          this.setState({
+            sdkAccount: sdkAccount
+          })
+        }
+      }
+    });
+
+    // For convinience add this to the account so you can close
+    // on componentWillUnmount. hat-tip to StellarTerm ;)
+    sdkAccount.close = () => {
+      try {
+        accountEventsClose();
+      } catch(e) {
+        console.log('error closing account streaming')
+      }
+    }
+
     this.setState({
       sdkAccount
     })
+  }
+
+  componentWillUnmount() {
+    const { sdkAccount } = this.state
+    // Close the stream when unmounting the component
+    sdkAccount.close()
   }
 
   render() {
