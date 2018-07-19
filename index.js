68583a6448adabd022b621c06ccfb7f94784a3c1
Adolfo Builes <builes.adolfo@gmail.com>
2018-07-18 20:38:23 -0500

    Add replacement for Node's core modules and globals.
    This is will allow you to use the Stellar SDK in React Native.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'node-libs-react-native/globals'
 import { AppRegistry } from 'react-native';
 import App from './App';
 AppRegistry.registerComponent('AnchorX', () => App);
--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,9 +1,12 @@
+const extraNodeModules = require('node-libs-react-native')
+extraNodeModules.vm = require.resolve('vm-browserify')
+
 module.exports = {
   getTransformModulePath() {
     return require.resolve("react-native-typescript-transformer");
   },
   getSourceExts() {
     return ["ts", "tsx"];
-  }
+  },
+  extraNodeModules
 };
-  
